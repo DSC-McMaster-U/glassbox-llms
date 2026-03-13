112fae041a895f7ce10ebb336b62500510279b42
Jason Cheng <jasonic.c.7134@gmail.com>
2026-03-13 18:39:19 -0400

    fix/src: modularize all preprocessing functions

--- glassboxllms/runner/preprocessing/__init__.py
+++ glassboxllms/runner/preprocessing/__init__.py
@@ -16,22 +16,34 @@ from .dataset import (
     select_columns,
 )
 from .start import start_preprocess
-from .tokenizers import tokenize_dataset
+from .tokenizers import (
+    batch_tokenize,
+    tokenize_and_align_labels,
+    tokenize_dataset,
+    tokenize_multimodal,
+    tokenize_with_encoding,
+    tokenize_with_special_tokens,
+    tokenize_with_truncation,
+)
 
 __all__ = [
-    # Tokenization
-    "tokenize_dataset",
-    # Dataset manipulation
-    "select_columns",
-    "rename_columns",
-    "sample_dataset",
-    # Text cleaning
+    # Dataset functions
+    "apply_transforms",
     "clean_text",
     "max_length",
     "min_length",
-    # General transformations
-    "apply_transforms",
     "normalize_text",
-    # init
+    "rename_columns",
+    "sample_dataset",
+    "select_columns",
+    # Preprocessing start
     "start_preprocess",
+    # Tokenizer functions
+    "batch_tokenize",
+    "tokenize_and_align_labels",
+    "tokenize_dataset",
+    "tokenize_multimodal",
+    "tokenize_with_encoding",
+    "tokenize_with_special_tokens",
+    "tokenize_with_truncation",
 ]
